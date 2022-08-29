6f69521408fab7cb95a0c1df099f06a9af6b04d0
JJOOYYONG <https://github.com/JJOOYYONG>
2022-08-29 21:11:33 +0900

    등록하기 작업

---
+++ src/pages/SceduleWritePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/common/Layout";
+import SceduleDay from "../components/SceduleDay";
+
+const SceduleWritePage = () => {
+  return (
+    <Layout>
+      <SceduleDay />
+    </Layout>
+  );
+};
+
+export default SceduleWritePage;
--- src/share/Router.js
+++ src/share/Router.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
-
+import ScedulePage from "../pages/SceduleWritePage";
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/write" element={<ScedulePage />} />
       </Routes>
     </BrowserRouter>
   );
