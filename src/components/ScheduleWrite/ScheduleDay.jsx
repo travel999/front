510af9ed46c1687433fd4d4e610a5e09123420bd
JJOOYYONG <https://github.com/JJOOYYONG>
2022-08-31 20:54:15 +0900

    등록하기 기능 추가

--- src/components/ScheduleWrite/ScheduleDay.jsx
+++ src/components/ScheduleWrite/ScheduleDay.jsx
@@ -3,49 +3,50 @@ import ScheduleWork from "../ScheduleWrite/ScheduleWork";
 
 const ScheduleList = () => {
   //state
-  const numVal = useRef(0);
-  const [index, setIndex] = useState(0);
-  const [dayNum, setDayNum] = useState([]);
-
+  const [index, setIndex] = useState();
+  const [dayNum, setDayNum] = useState([true]);
   //일차추가 버튼
   const addDay = () => {
-    const newDay = numVal;
-    console.log(newDay);
     // const newDay = Number(dayNum.slice(-1)[0]) + 1;
-    setDayNum([...dayNum, newDay]);
+    setDayNum([...dayNum, true]);
   };
   //일차삭제 버튼
   const removeDay = (targetDay) => {
-    if (targetDay === 1) {
+    if (targetDay === 0) {
       alert("1일차 일정은 삭제할 수 없습니다.");
     }
-    setDayNum(dayNum.filter((day) => day !== targetDay));
+    //선택한 일정의 값만 수정해야한다.
+    //useState의 불변성을 해칠 수 없어 복사배열을 생성 후 처리
+    let copyArray = [...dayNum];
+    copyArray[targetDay] = false;
+    setDayNum(copyArray);
   };
 
   return (
     <section className="dayWrap">
       <ul className="dayContent">
-        {dayNum.map((item) => (
-          <li
-            key={item}
-            className={index === item ? "active" : null}
-            onClick={() => setIndex(item)}
-          >
-            {item} 일차
-            {item === 1 ? null : (
-              <button id="removeBtn" onClick={() => removeDay(item)}>
-                X
-              </button>
-            )}
-          </li>
-        ))}
+        {dayNum
+          .filter((item) => item === true)
+          .map((item, i) => (
+            <li
+              key={i}
+              className={index === i ? "active" : null}
+              onClick={() => setIndex(i)}
+            >
+              {i + 1} 일차
+              {i + 1 === 1 ? null : (
+                <button id="removeBtn" onClick={() => removeDay(i)}>
+                  X
+                </button>
+              )}
+            </li>
+          ))}
       </ul>
       {dayNum
-        .filter((item) => index === item)
+        .filter((item, i) => item === true && index === i)
         .map((item) => (
-          <ScheduleWork key={item} day={item} />
+          <ScheduleWork key={item} day={index} />
         ))}
-      <input type="number" name="dayText" placeholder="일차지정" ref={numVal} />
       <button onClick={addDay}>일자 추가</button>
     </section>
   );
