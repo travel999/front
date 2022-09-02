1cb075890d1c14694b5be3e49a2e5cdec8a27cf3
JJOOYYONG <https://github.com/JJOOYYONG>
2022-09-02 22:03:54 +0900

    기능 수정

--- src/components/schedule/ScheduleWork.jsx
+++ src/components/schedule/ScheduleWork.jsx
@@ -4,15 +4,36 @@ import styels from "./Schedule.module.css";
 import Btn from "../elements/Btn";
 
 const ScheduleWrite = ({ day }) => {
+  //초기화 값
+  const initState = {
+    placeName: "",
+    locate: "",
+    content: "",
+  };
+
   //state
   const [cardNum, setCardNum] = useState([1]);
+  const [conData, setConData] = useState("");
+  const [listData, setListData] = useState(initState);
 
+  //함수
+  if (conData !== "") {
+    setListData(...listData, {
+      placeName: "123",
+      locate: "1234",
+      content: conData,
+    });
+    window.localStorage.setItem(day, listData);
+  }
+
+  //이벤트 함수
   //일정추가 버튼
-  const addWork = () => {
+  const onAddWork = () => {
     const newWork = Number(cardNum.slice(-1)[0]) + 1;
     setCardNum([...cardNum, newWork]);
   };
 
+  //입력한 값
   return (
     <div className={styels.worksWrap}>
       <h2>{day + 1}일차 우리가 갈 곳..!</h2>
@@ -23,18 +44,19 @@ const ScheduleWrite = ({ day }) => {
             type="text"
             name="work_title"
             className={styels.title}
-            placeholder="제목 입력"
+            placeholder="장소 선택"
             required
           />
           <textarea
-            name="work_cont"
+            name="content"
             className={styels.content}
             placeholder="일정 입력"
+            onChange={(e) => setConData(e.target.value)}
             required
           />
         </div>
       ))}
-      <Btn backgroundColor="gray" width="25px" onClick={addWork}>
+      <Btn backgroundColor="gray" width="25px" onClick={onAddWork}>
         +
       </Btn>
     </div>
