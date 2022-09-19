aaa25f15e0825413e276dba812618f013100e76d
whynotwhywhywhywhy <naomi981104@gmail.com>
2022-09-20 01:55:02 +0900

    [#95]일정수정기능 구현중

--- src/components/detailSchedule /DetailSchedule.jsx
+++ src/components/detailSchedule /DetailSchedule.jsx
@@ -1,16 +1,10 @@
-
-import React, { useEffect } from "react";
-
+import React, { useLayoutEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import io from "socket.io-client";
 
 import styels from "./Schedule.module.css";
-
 import Btn from "../elements/Btn";
 import NoDateDuckImg from "../../res/img/duck/noDateDuck.png";
-import { toast, ToastContainer } from "react-toastify";
-
 
 import DetailScheduleCreate from "./DetailScheduleCreate";
 import ScheduleCard from "./ScheduleCard";
@@ -20,26 +14,14 @@ import Chatting from "../chat/Chatting";
 // import { getSchedule } from "../../redux/modules/detailSchedule/DetailScheduleSlice";
 import { getSchedule } from "../../redux/modules/MapSlice";
 
-const socket = io.connect("http://52.78.142.77/", {
-  path: "/socket.io",
-  transports: ["websocket"],
-});
-
 const DetailSchedule = () => {
   const tokenValue = localStorage.getItem("jwtToken"); // 토크없으면 로그인 페이지로
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { id } = useParams();
-  const nickname = localStorage.getItem("nickname");
-  const [meaningless, setMeaningless] = useState(0);
 
-
-  const createData = useSelector((state) => state.schedule);
-  const mapData = useSelector((state) => state.kakaoMap);
-
-  //DB 가져오기
-  useEffect(() => {
-
+  //마운트 되기전에 저장된 DB 가져오기
+  useLayoutEffect(() => {
     if (!tokenValue) {
       navigate("/");
     } else {
@@ -47,17 +29,33 @@ const DetailSchedule = () => {
     }
   }, []);
 
+  const createData = useSelector((state) => state.schedule);
+  const mapData = useSelector((state) => state.kakaoMap);
+  console.log(mapData);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     makeDayArr();
+  //   }, 0)
+  // }, []);
+
+  // let dayData;
+
+  // const makeDayArr = () => {
+  //   const newData = [];
+  //   for (let i = 1; i <= 7; i++) {
+  //     //dbData에서 key값 동적할당
+  //     let days = "day" + i;
+  //     newData.push(dbData.data.data[days]);
+  //   }
+  //   return (dayData = newData.filter((item) => item !== undefined));
+  // };
 
   return (
     <div className={styels.wrap}>
       <div className={styels.wrapLeft}>
         <DetailScheduleCreate data={mapData} />
-        <ScheduleCard
-          data={mapData}
-          postId={id}
-          socket={socket}
-          SendOtherPeople={SendOtherPeople}
-        />
+        <ScheduleCard data={mapData} postId={id} />
       </div>
       <div className={styels.wrapCenter}>
         <ScheduleMap nowDay={mapData.day} data={mapData} />
