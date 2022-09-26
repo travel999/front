e65ba0399d8e4c69c88a3840b99b9313176d23d2
whynotwhywhywhywhy <naomi981104@gmail.com>
2022-09-27 00:06:02 +0900

    [#131][수정]

--- src/components/loginRegister/KakaoLogIn.jsx
+++ src/components/loginRegister/KakaoLogIn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const REST_API_KEY = "56b0ae6936317cb6d7059df454f97979"
-const REDIRECT_URI = "http://oorigachi.com/kakao/callback"
+const REDIRECT_URI = "https://oorigachi.com/kakao/callback"
 
 const KakaoLogIn = () => {
 
